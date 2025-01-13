463cf56215caf38e5a7c90a085e8f7d5fcc27003
Yogesh <yog7275@gmail.com>
2025-01-13 17:32:43 +0530

    asd

--- forms/contact.php
+++ forms/contact.php
@@ -4,12 +4,6 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-/**
- * Requires the "PHP Email Form" library
- * The "PHP Email Form" library is available only in the pro version of the template
- * The library should be uploaded to: vendor/php-email-form/php-email-form.php
- * For more info and help: https://bootstrapmade.com/php-email-form/
- */
 
 // Replace contact@example.com with your real receiving email address
 $receiving_email_address = 'yog7275@gmail.com';
